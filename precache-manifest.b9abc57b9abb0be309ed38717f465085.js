02b787c301b87b39fc08b847accb5041e055c6f8
unknown <garvitsaxena06@gmail.com>
2020-04-05 18:24:16 +0530

    Updates

---
+++ precache-manifest.b9abc57b9abb0be309ed38717f465085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c23bd323e70ac3b1ba978db44d32ec",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "274195307093a2a6b157",
+    "url": "/Todo-List/static/css/main.ef917d3f.chunk.css"
+  },
+  {
+    "revision": "1ddf748fb53f598ba407",
+    "url": "/Todo-List/static/js/2.b2821e01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List/static/js/2.b2821e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274195307093a2a6b157",
+    "url": "/Todo-List/static/js/main.a6f31508.chunk.js"
+  },
+  {
+    "revision": "91835c550e19201cfd34",
+    "url": "/Todo-List/static/js/runtime-main.5f4cb155.js"
+  }
+]);
